188dbe5ad2dc0be143266cada29ae81c4d15b7c1
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2025-11-25 10:07:16 +0000

    loops in py

---
+++ loops.py
@@ -0,0 +1,18 @@
+#while loop
+i = 1
+while i <= 6: 
+    print(i)
+    i+=1
+
+#for loop
+names = ['Sondos', 'Lina', 'Raghad'] #list
+for i in names: #for 'var' in 'lsitName'
+    print(i)
+
+#loop with range()
+for n in range(10):
+    print(n) #0 to 9
+for s in range(3,8):
+    print(s) #3 to 8-1=7
+for t in range(2, 11, 2):
+    print(t) #2 to 10 with step 2
