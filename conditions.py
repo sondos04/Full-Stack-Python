7d71b5ec2db207425be639ec459c896d964b7e9c
SondosAlrubaish <sondos.alrubaih04@gmail.com>
2025-11-25 09:43:07 +0000

    Condition if-elif-else

---
+++ conditions.py
@@ -0,0 +1,18 @@
+#if - else condition
+age = 15
+if age >= 18:
+    print("You are an adult!")
+else:
+    print("You are a minor!")
+
+#if - elif - else condition
+path = 'web'
+if path == 'android':
+    print('you use andriod')
+elif path == 'ios':
+    print('you use ios')
+elif path == 'web':
+    print('you use web')
+    print('you use python')
+else:
+    print('unknown path')
