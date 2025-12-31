aec1a830c2dcade90379ef7788fb7c278c1c0511
sondos04 <sondos.alrubaih04@gmail.com>
2025-12-31 10:40:18 +0300

    all about advance list in py

--- advancelist.py
+++ advancelist.py
@@ -3,4 +3,26 @@ age = [11, 15, 18, 20, 25, 30]
 def filtered_ages(age):
     return age >= 18
 
-print(list(filter(filtered_ages, age)))
+print(list(filter(filtered_ages, age))) #filer by the condition in the function, and print in list format
+
+def power_ages(age):
+    return age ** 2
+
+print(list(map(power_ages, age))) #powered by the condition in the function, and print in list format
+
+age1 = [11, 15, 18, 20, 25, 30]
+
+age1.sort() #يرتبها تصاعديا
+print(age1)
+
+age1.sort(reverse=True) #يرتبها تنازلي
+print(age1)
+
+age1.reverse() #يعكس ترتيبها من الاخير للاول
+print(age1)
+
+multilist = [num*2 for num in age1]
+print(multilist) #يضرب كل عنصر ب 2
+
+multilist = [num*2 for num in age1 if num > 20]
+print(multilist) #يضرب كل عنصر ب 2
